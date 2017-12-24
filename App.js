f988c27414e04570415570844a31f90c5eee3527
Jonathan Pucm <jonathancoder77@outlook.com>
2017-12-24 22:14:01 +1100

    add weather api

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import {
   Text,
   View,
   Platform,
-  TextInput,
   KeyboardAvoidingView,
-  ImageBackground
+  ImageBackground,
+  ActivityIndicator,
+  StatusBar
 } from 'react-native';
 
+import { fetchLocationId, fetchWeather } from './utils/api';
 import getImageForWeather from './utils/getImageForWeather';
 
 import SearchInput from './components/SearchInput';
@@ -18,34 +20,88 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      location: 'San Francisco'
+      location: '',
+      temperature: 0,
+      weather: '',
+      loading: false,
+      error: false
     };
   }
+
+  componentDidMount() {
+    this.handleUpdateLocation('San Francisco');
+  }
   render() {
-    const { location } = this.state;
+    const { loading, error, location, weather, temperature } = this.state;
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
         <ImageBackground
-          source={getImageForWeather('Showers')}
+          source={getImageForWeather(weather)}
           style={styles.imageContainer}
           imageStyle={styles.image}
         >
           <View style={styles.detailsContainer}>
-            <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
-            <Text style={[styles.smallText, styles.textStyle]}>Sunny</Text>
-            <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-            <SearchInput
-              placeholder="Search any city"
-              onSubmit={this.handleUpdateLocation}
-            />
+            <ActivityIndicator animating={loading} color="white" size="large" />
+
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different city.
+                  </Text>
+                )}
+
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {location}
+                    </Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>
+                      {weather}
+                    </Text>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {`${Math.round(temperature)}°`}
+                    </Text>
+                  </View>
+                )}
+
+                <SearchInput
+                  placeholder="Search any city"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
           </View>
         </ImageBackground>
       </KeyboardAvoidingView>
     );
   }
 
-  handleUpdateLocation = city => {
-    this.setState(() => ({ location: city }));
+  handleUpdateLocation = async city => {
+    if (!city) return;
+
+    this.setState(
+      () => ({ loading: true }),
+      async () => {
+        try {
+          const locationId = await fetchLocationId(city);
+          const { location, weather, temperature } = await fetchWeather(
+            locationId
+          );
+
+          this.setState(() => ({
+            loading: false,
+            error: false,
+            location,
+            weather,
+            temperature
+          }));
+        } catch (e) {
+          this.setState(() => ({ loading: false, error: true }));
+        }
+      }
+    );
   };
 }
 
